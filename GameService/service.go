7612ce1765e990eb0e23f783e0c854c7f1b566e9
jbenson <jj.benson@outlook.com>
2019-10-25 02:06:05 -0700

    Updates to Game Service - Add a RandomNumber struct and a helper function for getting the 3rd party response - Add helper to convert int to Choice - Decided to use Roll 2x rather than uroboros the /choice URL...needs research...

--- GameService/service.go
+++ GameService/service.go
@@ -8,10 +8,20 @@ import (
 	"net/http"
 	"runtime"
 	"strconv"
+	"time"
 
-	handlers "github.com/gorilla/handlers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
+
+//https://stackoverflow.com/questions/17156371/how-to-get-json-response-from-http-get
+// JJB - default client does not have a timeout so you probably don't want to use it on purpose.
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+// RandomNumber - expected response from [https://codechallenge.boohma.com/random]
+type RandomNumber struct {
+	RandomNumber int `json:"random_number"`
+}
 
 // Choice - Game options
 type Choice struct {
@@ -37,13 +47,40 @@ type Result struct {
 	Computer int    `json:"computer"`
 }
 
-// Choices - user can only select these.
+// Choices - users can only select these.
+// JJB - Are these secure here?  Can they change?
 var Choices = []Choice{
 	{1, "Rock"},
 	{2, "Paper"},
 	{3, "Scissors"},
 	{4, "Lizard"},
 	{5, "Spock"},
+}
+
+// Is lumping 3rd party services into packages a practical thing in Go?
+func rollRandomNumber() int {
+	pc, _, _, _ := runtime.Caller(0)
+	consoleLogEndpointHit(runtime.FuncForPC(pc).Name())
+
+	data := &RandomNumber{}
+
+	getResponse, err := myClient.Get("https://codechallenge.boohma.com/random")
+	if err != nil {
+		fmt.Println("err: ", err.Error())
+		return -1
+	}
+	defer getResponse.Body.Close()
+
+	//https://stackoverflow.com/questions/26744873/converting-map-to-struct
+
+	//go get github.com/mitchellh/mapstructure - is this required?
+	err2 := json.NewDecoder(getResponse.Body).Decode(&data)
+	if err2 != nil {
+		fmt.Println("err2: ", err2.Error())
+		return -2
+	}
+
+	return data.RandomNumber
 }
 
 func consoleLogEndpointHit(funcName string) {
@@ -57,7 +94,7 @@ func onHome(w http.ResponseWriter, r *http.Request) {
 	consoleLogEndpointHit(runtime.FuncForPC(pc).Name())
 
 	// Setup the body
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "JJB")
 }
 
 func onGetChoices(w http.ResponseWriter, r *http.Request) {
@@ -94,17 +131,41 @@ func onPlay(w http.ResponseWriter, r *http.Request) {
 	var playerChoice PlayerChoice
 	json.Unmarshal(reqBody, &playerChoice)
 
-	// TODO: who wins...
-	results := Result{"win", playerChoice.Player, 2}
+	var roll int = rollRandomNumber()
+
+	fmt.Println("data: ", roll)
+
+	var computerChoice Choice = rollToChoice(roll)
+
+	// TODO: figure out who actually wins...
+
+	results := Result{"win", playerChoice.Player, computerChoice.ID}
 	json.NewEncoder(w).Encode(results)
 }
 
+func rollToChoice(roll int) Choice {
+	switch {
+	case roll > 80:
+		return Choices[0]
+	case roll > 60:
+		return Choices[1]
+	case roll > 40:
+		return Choices[2]
+	case roll > 20:
+		return Choices[3]
+	default:
+		return Choices[4]
+	}
+}
 func onGetRandomChoice(w http.ResponseWriter, r *http.Request) {
 	pc, _, _, _ := runtime.Caller(0)
-	fmt.Println("Endpoint Hit: " + runtime.FuncForPC(pc).Name())
+	consoleLogEndpointHit(runtime.FuncForPC(pc).Name())
 
-	var response Choice
-	response = Choices[3]
+	var roll int = rollRandomNumber()
+
+	fmt.Println("data: ", roll)
+
+	var response Choice = rollToChoice(roll)
 
 	json.NewEncoder(w).Encode(response)
 }
@@ -129,6 +190,7 @@ func handleRequests() {
 	// Yes.  That's M*A*S*H*
 	log.Fatal(http.ListenAndServe(":4077", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
 
+	// JJB - Learn this [https://github.com/gorilla/mux#handling-cors-requests] because it explains how to do CORS without that handlers lib.
 }
 
 func main() {
